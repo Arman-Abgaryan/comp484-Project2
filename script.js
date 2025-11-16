28d193fe59134bf04fad27fa5a41d7c58193ff2b
Arman-Abgaryan <arman.abgaryan.091@my.csun.edu>
2025-11-15 18:45:29 -0800

    Updated files and added different functionalities

--- script.js
+++ script.js
@@ -7,33 +7,60 @@ $(function() { // Makes sure that your function is called once all the DOM eleme
     $('.treat-button').click(clickedTreatButton);
     $('.play-button').click(clickedPlayButton);
     $('.exercise-button').click(clickedExerciseButton);
-  
-
-  
+    $('.spin-button').click(clickedSpinButton);
     
   })
   
     // Add a variable "pet_info" equal to a object with the name (string), weight (number), and happiness (number) of your pet
-    var pet_info = {name:"My Pet Name", weight:"??", happiness:"??"};
+    var pet_info = {name: "Lucky", weight: 80, happiness: 50};
   
     function clickedTreatButton() {
       // Increase pet happiness
       // Increase pet weight
+      pet_info.happiness += 3; // When treat button is pressed, happiness is increased by 10
+      pet_info.weight += 2; // When treat button is pressed, weight is increased by 5
+
+      $("#petMessage").text("Yummy!").slideDown();
+      setTimeout(() => $("#petMessage").slideUp(), 2500);
       checkAndUpdatePetInfoInHtml();
     }
     
     function clickedPlayButton() {
       // Increase pet happiness
       // Decrease pet weight
+      pet_info.happiness += 4;
+      pet_info.weight -= 2;
+
+      $("#petMessage").text("I love to play!").slideDown();
+      setTimeout(() => $("#petMessage").slideUp(), 2500);
       checkAndUpdatePetInfoInHtml();
     }
     
     function clickedExerciseButton() {
       // Decrease pet happiness
       // Decrease pet weight
+      pet_info.happiness -= 5;
+      pet_info.weight -= 5;
+
+      $("#petMessage").text("Please, I don't want to exercise!").slideDown();
+      setTimeout(() => $("#petMessage").slideUp(), 2500);
+
       checkAndUpdatePetInfoInHtml();
     }
-  
+
+    function clickedSpinButton() {
+      pet_info.happiness += 3;
+      pet_info.weight -= 1;
+
+      $(".pet-image").addClass("spin-animation");
+
+      $("#petMessage").text("Lucky is spinning!").slideDown();
+
+      setTimeout(() => { $("#petMessage").slideUp(); }, 2500);
+
+      checkAndUpdatePetInfoInHtml();
+    }
+
     function checkAndUpdatePetInfoInHtml() {
       checkWeightAndHappinessBeforeUpdating();  
       updatePetInfoInHtml();
@@ -41,7 +68,14 @@ $(function() { // Makes sure that your function is called once all the DOM eleme
     
     function checkWeightAndHappinessBeforeUpdating() {
       // Add conditional so if weight is lower than zero.
+      if (pet_info.weight < 0) {
+         pet_info.weight = 0;
+      } 
+      if (pet_info.happiness < 0) {
+        pet_info.happiness = 0;
+      }
     }
+
     
     // Updates your HTML with the current values in your pet_info object
     function updatePetInfoInHtml() {
